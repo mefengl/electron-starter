16df77f47afaf2d7284ba9435a03227132686f37
不游 <71683364+mefengl@users.noreply.github.com>
2024-04-24 22:09:48 +0800

    clean up

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,41 +1,36 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
-
 function App(): JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
+      <div>Powered by electron-vite</div>
+      <div>
         Build an Electron app with
         {' '}
-        <span className="react">React</span>
+        <span>React</span>
         &nbsp;and
         {' '}
-        <span className="ts">TypeScript</span>
+        <span>TypeScript</span>
       </div>
-      <p className="tip">
+      <p>
         Please try pressing
         {' '}
         <code>F12</code>
         {' '}
         to open the devTool
       </p>
-      <div className="actions">
-        <div className="action">
+      <div>
+        <div>
           <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
             Documentation
           </a>
         </div>
-        <div className="action">
+        <div>
           <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
             Send IPC
           </a>
         </div>
       </div>
-      <Versions></Versions>
     </>
   )
 }
