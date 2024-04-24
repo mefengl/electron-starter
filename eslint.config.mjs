309528390f164d85a41e2b83c624072a48a05cdb
不游 <71683364+mefengl@users.noreply.github.com>
2024-04-24 21:54:26 +0800

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+export default antfu({}, ...compat.config({
+  extends: [
+    'plugin:tailwindcss/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/migration-from-tailwind-2': 'off',
+    'tailwindcss/enforces-shorthand': 'off',
+  },
+}))
