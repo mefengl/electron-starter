ddf3f1d87d52630f78cd059813723a7e7737affe
不游 <71683364+mefengl@users.noreply.github.com>
2024-07-12 05:42:31 +0800

    add eslint perfectionist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,21 @@
 import antfu from '@antfu/eslint-config'
 import { FlatCompat } from '@eslint/eslintrc'
+import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural'
 
 const compat = new FlatCompat()
 
 export default antfu({
   rules: {
+    'eslint-comments/no-unlimited-disable': 'off',
+    'import/order': 'off',
     'node/prefer-global/process': 'off',
+    'tailwindcss/migration-from-tailwind-2': 'off',
+    'tailwindcss/no-custom-classname': 'off',
   },
 }, ...compat.config({
   ignorePatterns: [
     'src/renderer/src/components/ui/*',
     'src/renderer/src/lib/utils.ts',
+    'tailwind.config.js',
   ],
-}), ...compat.config({
-  extends: [
-    'plugin:tailwindcss/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  rules: {
-    'tailwindcss/no-custom-classname': 'off',
-    'tailwindcss/migration-from-tailwind-2': 'off',
-    'tailwindcss/enforces-shorthand': 'off',
-  },
-}))
+}), perfectionistNatural)
